5b42206464c21729f35200e300405800cac7540e
khalid ibrahim <ibrahimkhalid478@gmail.com>
2021-01-27 21:52:14 +0000

    added slide show functionality to feedback

--- src/components/pages/FeedBack.js
+++ src/components/pages/FeedBack.js
@@ -1,40 +1,51 @@
-import React from 'react'
-import {EmojiEmotions} from '@material-ui/icons'
+import React, {useState} from 'react'
+import {EmojiEmotions, NavigateNext, NavigateBefore, Feedback} from '@material-ui/icons'
+import {Button} from '@material-ui/core'
+import Feedbacks from '../pages/feebackJson.json'
 import '../css/feedback.css'
 function FeedBack() {
-    const feedbacks = [
-        {
-            name : 'name 1',
-            para: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus est fugit sit excepturi aliquam expedita voluptate officiis dignissimos fugiat dolor?'
-        },
-        {
-            name : 'name 2',
-            para: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus est fugit sit excepturi aliquam expedita voluptate officiis dignissimos fugiat dolor?'
-        },
-        {
-            name : 'name 3',
-            para: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus est fugit sit excepturi aliquam expedita voluptate officiis dignissimos fugiat dolor?'
-        },
-        {
-            name : 'name 4',
-            para: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus est fugit sit excepturi aliquam expedita voluptate officiis dignissimos fugiat dolor?'
-        },
-    ]
+    const [count, setCount] = useState(0)
+    const [currentCard, setCurrentCard] = useState(Feedbacks)
 
-    console.log(feedbacks[1].name)
+    const nextHandler = (e) => {
+        e.preventDefault()
+        if(count < (currentCard.length - 1)){
+            setCount(count + 1)
+        }
+    }
+    const prevHandler = (e) => {
+        e.preventDefault()
+        if(count === 0){
+            setCount(0)
+        }else{
+            setCount(count - 1)
+        }
+    }
     
     return (
-        <div className="feedback" id="feedback">
-            <div className="feedback__container">
-                {
-                    feedbacks.map(cards => (
-                        <div className="content">
-                            <EmojiEmotions className="icon" />
-                            <h2>{cards.name}</h2>
-                            <p>{cards.para}</p>
-                        </div>
-                    ))
-                }
+        <div className="feedback__main">
+            <div className="feedback" id="feedback">
+                <div className="feedback__container">
+                    {
+                        // currentCard.map(cards => (
+                            <div className="content">
+                                <EmojiEmotions className="icon" />
+                                <h2>{currentCard[count].name}</h2>
+                                <p>{currentCard[count].para}</p>
+                            </div>
+                        // ))
+                    }
+                </div>
+            </div>
+            <div className="feedback__btn">
+                <div onClick={prevHandler} className="btn">
+                    <NavigateBefore />
+                    <Button className="prev">prev</Button>
+                </div>
+                <div onClick={nextHandler} className="btn">
+                    <Button className="next">next</Button>
+                    <NavigateNext />
+                </div>
             </div>
         </div>
     )
